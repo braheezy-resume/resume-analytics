0ee0488e3efd3dbedd40a9022efc7aa30a0efd5b
braheezy <michael.c.braha@gmail.com>
2023-04-17 10:07:52 -0700

    Remove headers that API Gateway should be handling

--- count.go
+++ count.go
@@ -46,15 +46,9 @@ func handleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 		return &events.APIGatewayProxyResponse{Body: "Oh no! Encountered an error", StatusCode: 400}, err
 	}
 
-	headers := map[string]string{
-		"Access-Control-Allow-Origin":  "*",
-		"Access-Control-Allow-Methods": "GET,PUT",
-	}
-
 	return &events.APIGatewayProxyResponse{
 			Body:       body,
-			StatusCode: 200,
-			Headers:    headers},
+			StatusCode: 200},
 		nil
 }
 
